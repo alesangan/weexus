b83d7e323f5889216537ef0eabeafdb1151bc41f
Alexander Sangan <alex@Alexanders-MacBook-Pro.local>
2017-04-26 19:49:56 +0100

    Added CanCan and role column to user model

--- weexus/app/controllers/posts_controller.rb
+++ weexus/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy], :authenticate_user!
+  before_action :authenticate_user!, :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
