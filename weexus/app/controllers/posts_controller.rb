48e4685cc461d58cd2ee38bb0192e9ecb246a66a
Grzegorz Szewczyk <grzeg.szewczyk@gmail.com>
2017-06-08 19:25:19 +0100

    empty post validation & search v1

--- weexus/app/controllers/posts_controller.rb
+++ weexus/app/controllers/posts_controller.rb
@@ -6,7 +6,12 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where(status: 'Done')
+    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search]).order("created_at DESC")
+    else
+      @posts = Post.all.order('created_at DESC')
+    end
   end
 
   def review
