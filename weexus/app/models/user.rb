4e4880ed48a03dd463ba094658a74cecd42a562d
Alexander Sangan <alex@Alexanders-MacBook-Pro.local>
2017-06-26 19:17:18 +0100

    Various fixes

--- weexus/app/models/user.rb
+++ weexus/app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   ROLES = %i[superadmin admin user]
   acts_as_voter
+  before_create :set_default_role
+
+  private
+  def set_default_role
+    self.role = 'user'
+  end
   # attr_accessor :terms_and_conditions
   # validates_acceptance_of :terms_and_conditions, :allow_nil => false, :message => ""
 end
